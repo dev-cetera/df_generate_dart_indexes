756aed636f8dba200201a741446917db1184d812
robmllze <86869208+robmllze@users.noreply.github.com>
2024-11-28 20:46:17 +1100

    update

--- lib/src/gen_indexes.dart
+++ lib/src/gen_indexes.dart
@@ -20,7 +20,6 @@ import 'package:path/path.dart' as p;
 const _DEFAULT_TEMPLATE_PATH_OR_URL =
     'https://raw.githubusercontent.com/robmllze/df_generate_dart_indexes/main/templates/template.dart.md';
 const _DEFAULT_OUTPUT_PATH = '{folder}.g.dart';
-const _PREFIX = '[gen-indexes]';
 
 // ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
 
@@ -65,8 +64,7 @@ Future<void> genIndexes(List<String> args) async {
   String outputFilePath;
   try {
     inputPath = argResults.option(DefaultOptions.INPUT_PATH.name)!;
-    templatePathOrUrl =
-        argResults.option(DefaultOptions.TEMPLATE_PATH_OR_URL.name)!;
+    templatePathOrUrl = argResults.option(DefaultOptions.TEMPLATE_PATH_OR_URL.name)!;
     outputFilePath = argResults.option(DefaultOptions.OUTPUT_PATH.name)!;
   } catch (_) {
     _print(
@@ -98,8 +96,7 @@ Future<void> genIndexes(List<String> args) async {
   // [STEP 7] Create a stream to get all files ending in .dart but not in
   // .g.dart and do not start with underscores.
   final filePathStream0 = PathExplorer(inputPath).exploreFiles();
-  final filePathStream1 =
-      filePathStream0.where((e) => _isAllowedFileName(e.path));
+  final filePathStream1 = filePathStream0.where((e) => _isAllowedFileName(e.path));
 
   final spinner = Spinner();
   spinner.start();
@@ -189,8 +186,7 @@ String _publicExports(
   bool Function(String filePath) test,
   String Function(String baseName) statementBuilder,
 ) {
-  final relativeFilePaths =
-      filePaths.map((e) => p.relative(e, from: inputPath));
+  final relativeFilePaths = filePaths.map((e) => p.relative(e, from: inputPath));
   final exportFilePaths = relativeFilePaths.where((e) => test(e));
   final statements = exportFilePaths.map(statementBuilder);
   return statements.join('\n');
